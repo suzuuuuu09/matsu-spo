74ffcf383930e55dbd2aec99a10a2f94efcd5510
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-09-02 14:30:08 +0900

    ✨ Markerのコンポーネント

--- app/components/Map.vue
+++ app/components/Map.vue
@@ -13,6 +13,8 @@
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
+
+      <Marker icon="lantern" :lat-lng="[35.6595, 139.7748]" />
     </l-map>
   </div>
 </template>
@@ -21,12 +23,14 @@
 // @ts-ignore
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import Marker from "@/components/Marker.vue";
 
 const props = defineProps<{
   url: string
   attribution: string
-}>()
+}>();
 
-const zoom = ref(15)
-const center = ref([35.6769883, 139.7588499])
+const iconUrl = new URL(`@/assets/img/lantern.png`, import.meta.url).href;
+const zoom = ref(15);
+const center = ref<[number, number]>([35.6769883, 139.7588499]);
 </script>
---
+++ app/components/Marker.vue
@@ -0,0 +1,21 @@
+<template>
+  <l-marker :lat-lng="latLng">
+    <l-icon
+      :icon-size="[32, 32]"
+      :icon-url="iconUrl"
+    />
+  </l-marker>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { LMarker, LIcon } from '@vue-leaflet/vue-leaflet';
+
+const props = defineProps<{
+  icon: string;
+  latLng: [number, number];
+}>();
+
+// publicフォルダの画像を参照
+const iconUrl = computed(() => `/img/${props.icon}.png`);
+</script>
