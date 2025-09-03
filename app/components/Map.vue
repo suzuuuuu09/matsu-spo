f89591075cff7aee9f9ee0aab3f85fdcc150ecb8
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-09-04 03:44:33 +0900

    🚀 test deploy

--- app/components/Map.vue
+++ app/components/Map.vue
@@ -1,16 +1,42 @@
 <template>
-  <MapContainer
-    :zoom="15"
-    :center="[35.18490, 136.89968]"
-    ref="mapContainer"
-  />
+  <div class="map-container">
+    <LMap
+      v-model:zoom="zoom"
+      :center="center"
+      :use-global-leaflet="false"
+      style="height: 100vh; width: 100%;"
+    >
+      <LTileLayer
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+      />
+      <LMarker
+        v-for="marker in markerItems"
+        :key="marker.name"
+        :lat-lng="marker.latLng"
+        @click="onMarkerClick(marker)"
+      >
+        <LIcon
+          :icon-url="`/img/icon/${marker.icon}.svg`"
+          :icon-size="[32, 32]"
+          :icon-anchor="[16, 32]"
+          :popup-anchor="[0, -32]"
+        />
+      </LMarker>
+    </LMap>
+  </div>
 </template>
 
 <script setup lang="ts">
-import MapContainer from "@/components/MapContainer.vue";
-import { MARKER_ITEMS } from "@/constants/marker";
-import type { MarkerItem } from "@/constants/marker";
-import { ref, onMounted, nextTick } from 'vue';
+import { ref } from 'vue';
+import { LMap, LTileLayer, LMarker, LIcon } from '@vue-leaflet/vue-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { MARKER_ITEMS } from '@/constants/marker';
+import type { MarkerItem } from '@/constants/marker';
+
+const zoom = ref(15);
+const center = ref<[number, number]>([35.18490, 136.89968]);
+const markerItems = MARKER_ITEMS;
 
 const props = defineProps<{
   isOpenSidebar: boolean;
@@ -21,43 +47,16 @@ const emit = defineEmits<{
   markerClick: [item: MarkerItem];
 }>();
 
-const mapContainer = ref();
-const markerItems = MARKER_ITEMS;
-
 // マーカーをクリックしたとき
 const onMarkerClick = (item: MarkerItem) => {
   emit('markerClick', item);
 };
-
-onMounted(async () => {
-  // MapContainerが読み込まれるまで待つ
-  await nextTick();
-  
-  // Leafletが利用可能になった後にマーカーを追加
-  setTimeout(async () => {
-    try {
-      const L = await import('leaflet');
-      const map = mapContainer.value?.getMap();
-      
-      if (map) {
-        // マーカーを追加
-        markerItems.forEach((item) => {
-          const customIcon = L.default.icon({
-            iconUrl: `/img/icon/${item.icon}.svg`,
-            iconSize: [32, 32],
-            iconAnchor: [16, 32],
-            popupAnchor: [0, -32]
-          });
-
-          const marker = L.default.marker(item.latLng, { icon: customIcon });
-          marker.on('click', () => onMarkerClick(item));
-          marker.addTo(map);
-        });
-      }
-    } catch (error) {
-      console.error('Failed to add markers:', error);
-    }
-  }, 1000);
-});
 </script>
+
+<style scoped>
+.map-container {
+  width: 100%;
+  height: 100vh;
+}
+</style>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@unocss/nuxt', '@nuxt/image'],
-  ssr: true,
+  ssr: false,
   nitro: {
     preset: 'cloudflare-pages',
     prerender: {
